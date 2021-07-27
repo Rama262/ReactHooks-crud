5131fa80682ea661dfad6b3df2defa7f1adaaf5e
9505985331 <tarakaramadevi@gmail.com>
2021-07-27 19:22:15 +0530

    first commit

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -7,20 +7,7 @@ const Navbar = () => {
         <Link className="navbar-brand" href="/">
           React crud
         </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse">
-          <ul className="navbar-nav mr-auto">
+        <ul className="navbar-nav mr-auto">
             <li className="nav-item">
               <NavLink className="nav-link" exact to="/">
                 View User
@@ -33,7 +20,9 @@ const Navbar = () => {
             </li>
           </ul>
         </div>
-      </div>
+
+
+     
     </nav>
   );
 };
